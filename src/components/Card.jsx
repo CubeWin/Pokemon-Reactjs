2bce9c0720ed2f313598e6355c86117ee458689e
Eliud Pedraza <eliud16pc@hotmail.com>
2022-05-27 23:47:34 -0500

    Consiguiendo imagenes por cada pokemon

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,36 @@
-const Card = ({ character }) => {
-	const { name, url } = character;
+import { useState, useEffect } from 'react';
+const Card = props => {
+	console.log(props);
+	const { name, url } = props.character;
+	const [character, setCharacter] = useState({});
+
+	useEffect(() => {
+		getCharacter();
+	}, []);
+
+	const getCharacter = async () => {
+		try {
+			const res = await fetch(url);
+			if (!res.ok) {
+				throw new Error('No se retorno res.ook');
+			}
+			const data = await res.json();
+			setCharacter(data);
+		} catch (err) {
+			console.log(`error => ${err}`);
+			return [{ error: err }];
+		}
+	};
+
 	return (
-		<div>
+		<div style={{ border: '1px solid #333', margin: '25px auto' }}>
 			<h1>{name}</h1>
 			<p>{url}</p>
+			<img
+				style={{ width: '150px', height: 'auto' }}
+				src={character.sprites.other.dream_world.front_default}
+			/>
 		</div>
 	);
 };
-
 export default Card;
